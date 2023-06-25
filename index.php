77df7e3e2d6ff109bbbae6585e3e09cacaece49a
tristan-badet <127488323+tristan-badet@users.noreply.github.com>
2023-06-25 16:22:53 +0200

    ajout des commentaires

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
+
+// Connexion à la base de données
+
 $db = new PDO ('mysql:host=localhost;dbname=tekshelf', 'root', '');
+
+
+// requete pour l'affichage des produits par nom et ordre alphabetique
 
 $queryByProductName = $db->prepare('SELECT * FROM stock ORDER BY nom_du_produit ASC');
 $queryByProductName->execute();
 $listByProductName = $queryByProductName->fetchall();
 
+// Requete pour l'affichage des produits selon le code bar, plus le code bar a un grand nombre, plus il est bas
+
 $queryByBarCode = $db->prepare('SELECT * FROM stock ORDER BY code_barre ASC');
 $queryByBarCode->execute();
 $listByBarCode = $queryByBarCode->fetchall();
+
+// Requete de la moyenne des stocks des produits
 
 $avgStockLeft = $db->prepare('SELECT AVG(stock_restant) AS average_stock_left FROM stock');
 $avgStockLeft->execute();
@@ -30,6 +40,7 @@ $displayAvgStockLeft = $rowAvgStockLeft['average_stock_left'];
             <h1>Affichage des produits par nom du produit</h1>
             <table>
                 <tr>
+                    <!-- Nom des colonnes du tableau -->
                     <th>ID</th>
                     <th>Nom du produit</th>
                     <th>Marque du produit</th>
@@ -37,6 +48,7 @@ $displayAvgStockLeft = $rowAvgStockLeft['average_stock_left'];
                     <th>Rayon</th>
                     <th>Stock restant</th>
                 </tr>
+                <!-- Pour chaque produit à l'intérieur de la base de données, le code PHP créé une ligne avec chaque information de la base de données -->
                 <?php foreach ($listByProductName as $value) { ?>
                 <tr>
                     <td><?php echo $value['id'];?></td>
@@ -62,6 +74,7 @@ $displayAvgStockLeft = $rowAvgStockLeft['average_stock_left'];
                     <th>Rayon</th>
                     <th>Stock restant</th>
                 </tr>
+                <!-- Pour chaque produit à l'intérieur de la base de données, le code PHP créé une ligne avec chaque information de la base de données -->
                 <?php foreach ($listByBarCode as $value) { ?>
                 <tr>
                     <td><?php echo $value['id'];?></td>
@@ -77,6 +90,7 @@ $displayAvgStockLeft = $rowAvgStockLeft['average_stock_left'];
     </section>
     <section>
         <div>
+            <!-- Affiche la moyenne des stocks -->
             <h1>Moyenne du stock des produits</h1>
             <h4><?php echo $displayAvgStockLeft;?></h4>
         </div>
